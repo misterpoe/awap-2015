99a8285ceb68a9adc7b66cc09706c3922690ea95
misterpoe <raymond@expii.com>
2015-10-17 16:40:38 -0400

    67k

--- src/game/player.py
+++ src/game/player.py
@@ -3,6 +3,7 @@ import random
 from base_player import BasePlayer
 from settings import *
 from norm import norm
+import sys
 
 class Player(BasePlayer):
     """
@@ -35,6 +36,35 @@ class Player(BasePlayer):
             self.paths.append(paths_from_src)
 
         return
+
+    def whereBuild(self, state, hub):
+        graph = state.get_graph()
+        nodes_cap = 100
+
+        total_nodes = 1
+        search_radius = 0
+        (best, bestDeg) = (hub, nx.degree(graph, hub))
+        S = set([hub])
+        visited = set()
+
+        # cap on the nodes we look at and the distance from the predicted hub
+        while total_nodes < nodes_cap and search_radius < min(ORDER_VAR/2, SCORE_MEAN/DECAY_FACTOR):
+            nextS = set()
+            for s in S:
+                if nx.degree(graph, s) > bestDeg and best not in self.stations:
+                    (best, bestDeg) = (s, nx.degree(graph, s))
+                visited.add(s)
+                for neighbor in nx.all_neighbors(graph, s):
+                    if neighbor not in visited:
+                        nextS.add(neighbor)
+                total_nodes += 1
+            search_radius += 1
+            S = nextS
+        return best
+
+    def shouldBuild(self, state):
+        expected_profit = (GAME_LENGTH-state.get_time())*ORDER_CHANCE*(SCORE_MEAN-ORDER_VAR*DECAY_FACTOR)/(len(self.stations)+1)
+        return expected_profit > INIT_BUILD_COST*(BUILD_FACTOR**len(self.stations))
 
     def reset_probs(self):
         self.probs = [0] * GRAPH_SIZE
@@ -82,11 +112,15 @@ class Player(BasePlayer):
         cost = INIT_BUILD_COST * (BUILD_FACTOR ** len(self.stations))
         if cost > state.get_money():
             return False
+        if len(self.stations) == 0:
+            return state.time > self.turnsToWait
         if self.total_orders_done_since_build == 0:
             return False
-        if float(len(self.missed_orders)) / self.total_orders_done_since_build > 0.5:
-            return True
-        return False
+        if float(len(self.missed_orders)) / self.total_orders_done_since_build <= 0.5:
+            return False
+        if not self.shouldBuild(state):
+            return False
+        return True
 
     def step(self, state):
         """
@@ -103,6 +137,7 @@ class Player(BasePlayer):
         """
         #recomputing
         pending_orders = state.get_pending_orders()
+        graph = state.get_graph()
         self.update_orders(state)
 
         if len(self.stations) == 0:
@@ -112,55 +147,49 @@ class Player(BasePlayer):
 
         self.guessedHubs = self.guessHubs()
         #print(self.guessedHubs)
-
         #after colecting some data
-        if state.time > self.turnsToWait:
-            graph = state.get_graph()
-            station = graph.nodes()[0]
+        commands = []
+        if self.should_build_station(state):
+            ideal_station = self.whereBuild(state, self.guessedHubs[0])
+            commands.append(self.build_command(ideal_station))
+            self.stations.append(ideal_station)
+            self.guessedHubs = []
+            self.missed_orders = []
+            self.total_orders_done_since_build = 0
+            self.reset_probs()
 
-            commands = []
+        new_graph = graph.copy()
+        for u, v in graph.edges():
+            if graph.edge[u][v]['in_use']:
+                new_graph.remove_edge(u, v)
 
-            for guess in self.guessedHubs:
-                if guess not in self.stations:
-                    if self.should_build_station(state):
-                        commands.append(self.build_command(guess))
-                        self.stations.append(guess)
-                        self.guessedHubs = []
-                        self.missed_orders = []
-                        self.total_orders_done_since_build = 0
-                        self.reset_probs()
+        #print len(self.stations)
+        self.prev_orders = pending_orders
 
-            new_graph = graph.copy()
-            for u, v in graph.edges():
-                if graph.edge[u][v]['in_use']:
-                    new_graph.remove_edge(u, v)
+        random.shuffle(pending_orders)
 
-            #print len(self.stations)
-            self.prev_orders = pending_orders
+        sys.stderr.write('%s' % self.stations)
+        #print(self.stations, file=sys.stderr)
 
-            random.shuffle(pending_orders)
+        for order in pending_orders:
+            bestPath = None
+            bestLength = 9999
+            for station in self.stations:
+                if not nx.has_path(new_graph, station, order.get_node()):
+                    continue
+                apath = nx.shortest_path(new_graph, station, order.get_node())
 
-            print(self.stations)
+                #print(len(apath))
+                if bestPath == None or len(apath) < len(bestPath):
+                    bestPath = apath
+                    bestLength = len(apath)
 
-            for order in pending_orders:
-                bestPath = None
-                bestLength = 9999
-                for station in self.stations:
-                    if not nx.has_path(new_graph, station, order.get_node()):
-                        continue
-                    apath = nx.shortest_path(new_graph, station, order.get_node())
+            if bestPath and bestLength * DECAY_FACTOR < order.money:
+                commands.append(self.send_command(order, bestPath))
+                self.fulfilled_orders.append(order)
+                for i in range(len(bestPath) - 1):
+                    new_graph.remove_edge(bestPath[i], bestPath[i + 1])
 
-                    #print(len(apath))
-                    if bestPath == None or len(apath) < len(bestPath):
-                        bestPath = apath
-                        bestLength = len(apath)
-
-                if bestPath and bestLength * DECAY_FACTOR < order.money:
-                    commands.append(self.send_command(order, bestPath))
-                    self.fulfilled_orders.append(order)
-                    for i in range(len(bestPath) - 1):
-                        new_graph.remove_edge(bestPath[i], bestPath[i + 1])
-
-                return commands
+            return commands
 
         return []
